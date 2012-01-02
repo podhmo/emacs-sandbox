797d3eb1931c615251a4d9e85ddd7219ddeadb4f
podhmo <ababjam61@gmail.com>
2012-01-02 23:46:14 +0900

    add python1

--- Makefile
+++ Makefile
@@ -10,7 +10,11 @@ check_env:
 run: check_env
 	 ! test -f $(ENV)/.downloaded && (pushd $(ENV) && . download && popd) || echo "already exist"
 	 emacs -Q -l $(ENV)/init.el &
-	 
+
+clean: check_env
+	 test -d $(ENV)/3rdparty && rm -r $(ENV)/3rdparty
+	 test -f $(ENV)/.downloaded && rm $(ENV)/.downloaded
+
 install: check_env check_home
 	 readlink $(HOME)/.emacs.d && rm $(HOME)/.emacs.d || echo "$(HOME)/.emacs.d is not found"
 	 ln -s `realpath $(ENV)` $(HOME)/.emacs.d
---
+++ python1/init.el
@@ -0,0 +1,66 @@
+(defun current-directory ()
+  (if load-file-name
+      (file-name-directory load-file-name)
+      default-directory))
+
+;; 行数表示
+(line-number-mode t)
+
+;; スタートアップページを表示しない
+(setq inhibit-startup-message t)
+
+;; バックアップファイルを作らない
+(setq backup-inhibited t)
+
+;; Macのキーバインドを使う。optionをメタキーにする。
+(when (boundp 'mac-key-mode)
+  (mac-key-mode 1)
+  (setq mac-option-modifier 'meta))
+
+;; タブキー
+(setq default-tab-width 4)
+(setq indent-line-function 'indent-relative-maybe)
+(setq-default indent-tabs-mode nil)
+
+;; シフト + 矢印で範囲選択
+(setq pc-select-selection-keys-only t)
+(pc-selection-mode 1)
+
+;; メニューバーを隠す
+(tool-bar-mode -1)
+
+;;; emacsclient サーバを起動
+(when (and (require 'server nil t) (not (server-running-p)))
+  (server-start))
+
+;;; 圧縮されたファイルも編集できるようにする
+(auto-compression-mode t)
+
+;;; バックアップファイルを作らない
+(setq backup-inhibited t)
+
+;;; 対応する括弧を光らせる。
+(show-paren-mode 1)
+
+;;; ホイールマウス
+(mouse-wheel-mode t)
+(setq mouse-wheel-follow-mouse t)
+
+;;; カーソルの位置が何文字目かを表示する
+(column-number-mode t)
+;;; カーソルの位置が何行目かを表示する
+(line-number-mode t)
+
+;;; clipboard共有
+(setq x-select-enable-clipboard t)
+
+;; python
+(add-to-list 'load-path (concat (current-directory) "3rdparty"))
+(add-to-list 'load-path (concat (current-directory) "3rdparty/auto-complete-1.3.1"))
+(require 'auto-complete nil t)
+(global-auto-complete-mode t)
+
+(require 'python-mode nil t)
+(add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(require 'ac-python nil t)
