aa247a577bf8f007df490261b68275e15880343c
podhmo <podhmo@podhmo.jp>
2013-04-22 21:58:54 +0900

    -

--- my/init.el
+++ my/init.el
@@ -245,6 +245,8 @@
               (lambda ()
                 (autopair-on)
                 (modify-syntax-entry ?% "w_"))))
+  (named-progn scheme
+    (load "language.scheme"))
 
   (named-progn python 
     (load "language.python")
@@ -319,10 +321,15 @@
 
 (add-to-list 'exec-path (concat (my:haskell-cabal-home) "/bin"))
 
-(defun my:haskell-setup ()
-  (ghc-init)
-  (flymake-mode)
-  )
+(custom-set-variables
+ '(haskell-mode-hook '(turn-on-haskell-indentation)))
+
+;; (defun my:haskell-setup ()
+;;   ;; (ghc-init)
+;;   (flymake-mode)
+;;   )
+;; (add-hook 'haskell-mode-hook 'my:haskell-setup)
+
 ;; (autoload 'ghc-init "ghc" nil t)
 ;; (ac-define-source ghc-mod
 ;;   '((depends ghc)
@@ -330,13 +337,15 @@
 ;;     (symbol . "s")
 ;;     (cache)))
 
-(defun my:ac-haskell-mode ()
-  (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-dictionary ac-source-ghc-mod)))
-(add-hook 'haskell-mode-hook 'my:ac-haskell-mode)
+;; (defun my:ac-haskell-mode ()
+;;   (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-dictionary ac-source-ghc-mod)))
+;; (add-hook 'haskell-mode-hook 'my:ac-haskell-mode)
 
-(defun my:haskell-ac-init ()
-  (when (member (file-name-extension buffer-file-name) '("hs" "lhs"))
-    (auto-complete-mode t)
-    (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-dictionary ac-source-ghc-mod))))
+;; (defun my:haskell-ac-init ()
+;;   (when (member (file-name-extension buffer-file-name) '("hs" "lhs"))
+;;     (auto-complete-mode t)
+;;     (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-dictionary ac-source-ghc-mod))))
 
-(add-hook 'find-file-hook 'my:haskell-ac-init)
+;; (add-hook 'find-file-hook 'my:haskell-ac-init)
+(put 'narrow-to-region 'disabled nil)
+
