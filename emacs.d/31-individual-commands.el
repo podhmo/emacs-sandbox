ba3b65b5c6c96a04137c41f28f50b21c22467b1f
podhmo <ababjam61+github@gmail.com>
2019-08-11 16:49:45 +0900

    ipsum

--- emacs.d/31-individual-commands.el
+++ emacs.d/31-individual-commands.el
@@ -241,3 +241,7 @@ so. c-u 3 follow-with-n-window, then a frame splitted 3window
   (interactive)
   (browse-github :branch "master"))
 
+(defun my:ipsum ()
+  (interactive)
+  (insert "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+  )
