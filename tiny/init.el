0805750c9d125c42c9d3719e462fb68182a9a791
podhmo <ababjam61+github@gmail.com>
2024-04-05 19:27:29 +0900

    refactor: init.elに収まる範囲なきがする

--- tiny/init.el
+++ tiny/init.el
@@ -204,25 +204,55 @@
 
   )
 
-(defun my:delete-something () (interactive)
-       (cl-dolist (thing '(symbol word))
-	 (when-let ((found (thing-at-point thing)))
-	   (cl-return (delete-region (beginning-of-thing thing)  (end-of-thing thing ))))))
+(progn ;; text-editing
+  (defun my:delete-something () (interactive)
+         (cl-dolist (thing '(symbol word))
+	   (when-let ((found (thing-at-point thing)))
+	     (cl-return (delete-region (beginning-of-thing thing)  (end-of-thing thing ))))))
 
-(defun my:enclose-quote (beg end)
-  "foo -> \"foo\""
-  (interactive
-   (list
-    (if (use-region-p) (region-beginning) (beginning-of-thing 'word))
-    (if (use-region-p) (region-end) (end-of-thing 'word))))
-  (save-restriction
-    (let ((text (buffer-substring-no-properties beg end)))
-      (delete-region beg end)
-      (insert (prin1-to-string text))))  ;; prin1-to-string ha tenuki
+  (defun my:enclose-quote (beg end)
+    "foo -> \"foo\""
+    (interactive
+     (list
+      (if (use-region-p) (region-beginning) (beginning-of-thing 'word))
+      (if (use-region-p) (region-end) (end-of-thing 'word))))
+    (save-restriction
+      (let ((text (buffer-substring-no-properties beg end)))
+        (delete-region beg end)
+        (insert (prin1-to-string text))))  ;; prin1-to-string ha tenuki
+    )
   )
 
 (progn ;; code reading
-  (autoload 'my:browse-github (concat (current-directory) "browse-github.el"))
+  (cl-defun my:browse-github (&key (branch nil) (rel-path nil) (line-no nil))
+    (interactive)
+    (and-let*
+        ((root  (vc-git-root default-directory))
+         (url (my:resolve-github-url  (format "%s.git/config" root ))))
+      (let* ((branch (or branch (car (vc-git-branches))))
+             (rel-path (or rel-path (replace-regexp-in-string (expand-file-name  root) "" (buffer-file-name))))
+             (line-no (or line-no (line-number-at-pos)))
+             (full-url (format "%s/blob/%s/%s#L%d" url branch rel-path line-no)))
+        (browse-url full-url))))
+
+  (defun my:resolve-github-url (git-config-file)
+    (cl-block b
+      (let ((buf (find-file-noselect git-config-file)))
+        (with-current-buffer buf
+          (goto-char (point-min))
+          (when (re-search-forward "url = git@github.com:\\(.+\\)\\(?:\\.git\\)?" nil t)
+            (let ((user-repository-name (string-trim-right (match-string-no-properties 1) "\\.git$")))
+              (cl-return-from b (format "https://github.com/%s" user-repository-name))))
+          (goto-char (point-min))
+          (when (re-search-forward "url = ssh://git@github.com/\\(.+\\)\\(?:\\.git\\)?" nil t)
+            (let ((user-repository-name (string-trim-right (match-string-no-properties 1) "\\.git$")))
+              (cl-return-from b (format "https://github.com/%s" user-repository-name))))
+          (goto-char (point-min))
+          (when (re-search-forward "url = https://github.com/\\(.+\\)\\(?:\\.git\\)?" nil t)
+            (let ((user-repository-name (string-trim-right (match-string-no-properties 1) "\\.git$")))
+              (cl-return-from b (format "https://github.com/%s" user-repository-name))))
+          ))))
+
   (defalias 'browse-github 'my:browse-github)
   (defun browse-github-master () (interactive)
          (browse-github :branch "master"))
