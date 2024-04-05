2f96410271950a6e84ea51108cfe19044b9ab651
podhmo <ababjam61+github@gmail.com>
2024-04-05 19:05:05 +0900

    refactor: 機能を限定してbrowse-githubをシンプルにした

--- tiny/browse-github.el
+++ tiny/browse-github.el
@@ -1,38 +1,31 @@
 ;;; -*- coding: utf-8; lexical-binding: t -*-
-
-;; (require 'pickup)
-(autoload 'pickup:pickup-file (concat (current-directory) "pickup.el"))
+(require 'cl-lib)
 
 (cl-defun my:browse-github (&key (branch nil) (rel-path nil) (line-no nil))
   (interactive)
-  (cl-destructuring-bind (_ . path) (project-current)
-    (when-let* ((url (my:resolve-github-url path)))
-      (let* ((branch (or branch (car (vc-git-branches))))
-             (rel-path (or rel-path (replace-regexp-in-string (expand-file-name path) "" (buffer-file-name))))
-             (line-no (or line-no (line-number-at-pos)))
-             (full-url (format "%s/blob/%s/%s#L%d" url branch rel-path line-no)))
-        (browse-url full-url))))
-  )
+  (and-let*
+      ((root  (vc-git-root default-directory))
+       (url (my:resolve-github-url  (format "%s.git/config" root ))))
+    (let* ((branch (or branch (car (vc-git-branches))))
+           (rel-path (or rel-path (replace-regexp-in-string (expand-file-name  root) "" (buffer-file-name))))
+           (line-no (or line-no (line-number-at-pos)))
+           (full-url (format "%s/blob/%s/%s#L%d" url branch rel-path line-no)))
+      (browse-url full-url))))
 
-(defun my:resolve-github-url (&optional path)
-  (when-let* ((git-config-file (pickup:pickup-file ".git/config" :path path)))
-    (cl-block b
-      (let ((buf (find-file-noselect git-config-file)))
-        (with-current-buffer buf
-          (goto-char (point-min))
-          (when (re-search-forward "url = git@github.com:\\(.+\\)\\(?:\\.git\\)?" nil t)
-            (let ((user-repository-name (string-trim-right (match-string-no-properties 1) "\\.git$")))
-              (cl-return-from b (format "https://github.com/%s" user-repository-name))))
-          (goto-char (point-min))
-          (when (re-search-forward "url = ssh://git@github.com/\\(.+\\)\\(?:\\.git\\)?" nil t)
-            (let ((user-repository-name (string-trim-right (match-string-no-properties 1) "\\.git$")))
-              (cl-return-from b (format "https://github.com/%s" user-repository-name))))
-          (goto-char (point-min))
-          (when (re-search-forward "url = https://github.com/\\(.+\\)\\(?:\\.git\\)?" nil t)
-            (let ((user-repository-name (string-trim-right (match-string-no-properties 1) "\\.git$")))
-              (cl-return-from b (format "https://github.com/%s" user-repository-name))))
-          )))))
-
-
-
-
+(defun my:resolve-github-url (git-config-file)
+  (cl-block b
+    (let ((buf (find-file-noselect git-config-file)))
+      (with-current-buffer buf
+        (goto-char (point-min))
+        (when (re-search-forward "url = git@github.com:\\(.+\\)\\(?:\\.git\\)?" nil t)
+          (let ((user-repository-name (string-trim-right (match-string-no-properties 1) "\\.git$")))
+            (cl-return-from b (format "https://github.com/%s" user-repository-name))))
+        (goto-char (point-min))
+        (when (re-search-forward "url = ssh://git@github.com/\\(.+\\)\\(?:\\.git\\)?" nil t)
+          (let ((user-repository-name (string-trim-right (match-string-no-properties 1) "\\.git$")))
+            (cl-return-from b (format "https://github.com/%s" user-repository-name))))
+        (goto-char (point-min))
+        (when (re-search-forward "url = https://github.com/\\(.+\\)\\(?:\\.git\\)?" nil t)
+          (let ((user-repository-name (string-trim-right (match-string-no-properties 1) "\\.git$")))
+            (cl-return-from b (format "https://github.com/%s" user-repository-name))))
+        ))))
