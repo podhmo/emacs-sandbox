1262955a78502d4f25c50b0f21084ab4a3f518a8
podhmo <ababjam61+github@gmail.com>
2024-04-16 15:54:30 +0900

    refactor: layoutの設定の方もsystem-typeでの分岐をわかりやすく

--- tiny/init.el
+++ tiny/init.el
@@ -67,27 +67,33 @@
 
 ;; layout
 (progn
-  (setq-default line-spacing 0.03) ;; すこしだけ行間にスペースをいれる
+  (defun my:setup-layout--mac ()
+    (setq-default line-spacing 0.03) ;; すこしだけ行間にスペースをいれる
 
-  (progn ; window-layout (frame layout in emacs's glossary)
-    (pcase system-type
-      ('darwin
-       (dolist (x '((top . 90) (left . 80) (width . 176) (height . 41) (left-fringe 12) (right-fringe 12)))
-	 (add-to-list 'default-frame-alist x))
-       )
-      ('gnu/linux ; wsl
-       ;; window
-       (dolist (x '((top . 0) (left . 80) (width . 176) (height . 39) (left-fringe 12) (right-fringe 12)))
-	 (add-to-list 'default-frame-alist x))
-       ;; emoji display
-       ;; need: apt-get install fonts-noto
-       ;; see: https://ianyepan.github.io/posts/emacs-emojis/
-       (and-let* ((its (member "Noto Color Emoji" (font-family-list))))
-	 (custom-set-faces
-	  '(default ((t (:family "Noto Sans CJK JP" :foundry "GOOG" :slant normal :weight normal :height 120 :width normal)))))
-	 (set-fontset-font t 'symbol (font-spec :family (car its)) nil 'prepend))
-       )
-      )
+    (dolist (x '((top . 90) (left . 80) (width . 176) (height . 41) (left-fringe 12) (right-fringe 12)))
+      (add-to-list 'default-frame-alist x)) )
+
+  (defun my:setup-layout--windows ()
+    (setq-default line-spacing 0.03) ;; すこしだけ行間にスペースをいれる
+
+    (dolist (x '((top . 0) (left . 80) (width . 176) (height . 39) (left-fringe 12) (right-fringe 12)))
+      (add-to-list 'default-frame-alist x))
+    ;; emoji display
+    ;; need: apt-get install fonts-noto
+    ;; see: https://ianyepan.github.io/posts/emacs-emojis/
+    (and-let* ((its (member "Noto Color Emoji" (font-family-list))))
+      (custom-set-faces
+       '(default ((t (:family "Noto Sans CJK JP" :foundry "GOOG" :slant normal :weight normal :height 120 :width normal)))))
+      (set-fontset-font t 'symbol (font-spec :family (car its)) nil 'prepend))
+    )
+
+  ;; window-layout (frame layout in emacs's glossary)
+  (pcase system-type
+    ('darwin
+     (my:setup-layout--mac))
+    ('gnu/linux ; wsl
+     (my:setup-layout--windows ))
+    (typ (message "layout setting is not found. system-type='%S" typ))
     )
 
   ;; theme
@@ -109,8 +115,10 @@
     (display-time-mode t)
     )
 
-  ;; (menu-bar-mode)
-  (tool-bar-mode -1)
+  (progn ; bar
+    ;; (menu-bar-mode)
+    (tool-bar-mode -1)
+    )
   )
 
 (progn ; tab-bar
