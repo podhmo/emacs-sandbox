e7b5788ac66eb61259b8a6bfd31f05b4f8a0bdea
podhmo <ababjam61+github@gmail.com>
2024-05-04 21:27:10 +0900

    chore: emacs27用のコードを削除

--- tiny/macros.el
+++ tiny/macros.el
@@ -8,10 +8,6 @@
   `(let ((it ,test-form))
      (if it ,then-form ,@else-forms)))
 
-(unless (fboundp 'and-let*)
-  (require 'subr-x) ; for emacs 27.1
-  )
-
 
 (defmacro def-toggle (name &rest body)
   (and-let* ((on-clause (aif (assoc-default :on body) `(progn ,@it)))
