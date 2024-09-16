b20194f4b5a4f2a4a5820f569868074dd4b03097
podhmo <ababjam61+github@gmail.com>
2024-09-17 00:19:25 +0900

    chore: Makfileで邪魔な回答を潰す

--- tiny/init.el
+++ tiny/init.el
@@ -153,6 +153,26 @@
 
     )
 
+  (progn ; make-mode
+    (with-eval-after-load 'make-mode
+      ;; auto-save中で定期的にy-or-n-pで尋ねてくるのはうるさすぎるので *Messages* に書くだけにする
+      (defun makefile-warn-suspicious-lines ()
+        ;; Returning non-nil cancels the save operation
+        (if (derived-mode-p 'makefile-mode)
+            (save-excursion
+	      (goto-char (point-min))
+	      (if (re-search-forward "^\\(\t+$\\| +\t\\)" nil t)
+	          (message "Suspicious line %d. Save anyway? "
+			   (count-lines (point-min) (point)))))))
+      (defun makefile-warn-continuations ()
+        (if (derived-mode-p 'makefile-mode)
+            (save-excursion
+	      (goto-char (point-min))
+	      (if (re-search-forward "\\\\[ \t]+$" nil t)
+		  (message "Suspicious continuation in line %d. Save anyway? "
+			   (count-lines (point-min) (point)))))))
+      ))
+
   (progn ; javascript-mode
     (add-to-list  'auto-mode-alist '("\\.mjs" .  js-mode))
 
